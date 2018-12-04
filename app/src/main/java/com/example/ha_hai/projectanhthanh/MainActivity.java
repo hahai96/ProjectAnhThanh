73ae6ca1b970a0b9e5f266481bb690e9531d7f48
Ha Hai <hasonhai1996@gmail.com>
2018-12-04 21:21:59 +0700

    man hinh 2

--- app/src/main/java/com/example/ha_hai/projectanhthanh/MainActivity.java
+++ app/src/main/java/com/example/ha_hai/projectanhthanh/MainActivity.java
@@ -2,20 +2,33 @@ package com.example.ha_hai.projectanhthanh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_place;
+    TextView tv_admob;
+    RecyclerView rv_link_invitee_detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_list_guest);
 
-        tv_place = findViewById(R.id.tv_place);
-        tv_place.setText(Html.fromHtml(getString(R.string.location)));
+//        tv_place = findViewById(R.id.tv_place);
+//        tv_place.setText(Html.fromHtml(getString(R.string.location)));
+//        tv_admob = findViewById(R.id.tv_admob);
+//        tv_admob.setText(Html.fromHtml(getString(R.string.tv_admod)));
+
+
+        rv_link_invitee_detail = findViewById(R.id.rv_guest);
+        rv_link_invitee_detail.setHasFixedSize(true);
+        rv_link_invitee_detail.setLayoutManager(new LinearLayoutManager(this));
+        rv_link_invitee_detail.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        rv_link_invitee_detail.setAdapter(new MyRecyclerAdapter(this));
     }
 }
 
