8c2145664d6402ebe43a0580aeb22288060f1703
ha hai <hasonhai1996@gmail.com>
2018-12-31 00:18:29 +0700

    màn hình 9

--- app/src/main/java/com/example/ha_hai/projectanhthanh/MainActivity.java
+++ app/src/main/java/com/example/ha_hai/projectanhthanh/MainActivity.java
@@ -3,41 +3,66 @@ package com.example.ha_hai.projectanhthanh;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     MyRecyclerAdapter mAdapterInvitee;
-    MyRecyclerAdapter mAdapterQuestions;
 
-    RecyclerView rv_person;
-    RecyclerView mRvQuestions;
+    RecyclerView rv_duong_family;
+
+    Button btAccept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_change_infor);
+        setContentView(R.layout.fragment_learn_about_duong_family);
         ButterKnife.bind(this);
 
-        rv_person = findViewById(R.id.rv_change_infor);
-        rv_person.setHasFixedSize(true);
-        rv_person.setLayoutManager(new LinearLayoutManager(this));
-        rv_person.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        mAdapterInvitee = new MyRecyclerAdapter(this, R.layout.item_information);
-        rv_person.setAdapter(mAdapterInvitee);
+        rv_duong_family = findViewById(R.id.rv_duong_family);
+        rv_duong_family.setHasFixedSize(true);
+        rv_duong_family.setLayoutManager(new GridLayoutManager(this, 2));
+        rv_duong_family.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        mAdapterInvitee = new MyRecyclerAdapter(this, R.layout.item_duong_family);
+        rv_duong_family.setAdapter(mAdapterInvitee);
 
-//        mRvQuestions = findViewById(R.id.rv_question);
-//        mRvQuestions.setHasFixedSize(true);
-//        mRvQuestions.setLayoutManager(new LinearLayoutManager(this));
-//        mRvQuestions.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-//        mAdapterQuestions = new MyRecyclerAdapter(this, R.layout.row_question);
-//        mRvQuestions.setAdapter(mAdapterQuestions);
+//        btAccept = findViewById(R.id.bt_accept);
+//        btAccept.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                TimePickerDialog mDialogAll = new TimePickerDialog.Builder()
+//                        .setCallBack(new OnDateSetListener() {
+//                            @Override
+//                            public void onDateSet(TimePickerDialog timePickerView, long millseconds) {
 //
-//        AddPhotoBottomDialogFragment addPhotoBottomDialogFragment = AddPhotoBottomDialogFragment.newInstance();
-//        addPhotoBottomDialogFragment.show(getSupportFragmentManager(), "add_photo_dialog_fragment");
+//                            }
+//                        })
+//                        .setCancelStringId("Cancel")
+//                        .setSureStringId("Sure")
+//                        .setTitleStringId("TimePicker")
+//                        .setYearText("Year")
+//                        .setMonthText("Month")
+//                        .setDayText("Day")
+//                        .setHourText("Hour")
+//                        .setMinuteText("Minute")
+//                        .setCyclic(false)
+//                        .setMinMillseconds(System.currentTimeMillis())
+//                        .setMaxMillseconds(System.currentTimeMillis() + 10000)
+//                        .setCurrentMillseconds(System.currentTimeMillis())
+//                        .setThemeColor(getResources().getColor(R.color.timepicker_dialog_bg))
+//                        .setType(Type.ALL)
+//                        .setWheelItemTextNormalColor(getResources().getColor(R.color.timetimepicker_default_text_color))
+//                        .setWheelItemTextSelectorColor(getResources().getColor(R.color.timepicker_toolbar_bg))
+//                        .setWheelItemTextSize(12)
+//                        .build();
+//            }
+//        });
     }
 }
 
